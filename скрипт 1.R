3ac68147ae635ac4fb4d83053a8bfeea4bd5f8df
Jane <malyatovaanastasia@yandex.ru>
2021-11-18 11:24:01 +0300

    1 скрипт

---
+++ скрипт 1.R
@@ -0,0 +1,14 @@
+install.packages("readr")
+mutations <- read_table("mutations.txt")
+clinical_data <- read_table("combined_study_clinical_data.tsv")
+protein <- read_table("Protein expression z-scores (RPPA).txt")
+expression <- read_table("mRNA expression z-scores relative to normal samples (log RNA Seq V2 RSEM).txt")
+
+
+
+
+
+
+
+
+
